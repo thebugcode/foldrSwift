91fe9ec255ab7f2b9d9fe96bf4b534e62be0e9ea
Dan Ursu <dan.ursu@yopeso.com>
2015-08-28 22:31:30 +0300

    Added examples of sum, count and map using foldr

---
+++ asdas/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  asdas
+//
+//  Created by Dan Ursu on 26/08/15.
+//  Copyright (c) 2015 Dan Ursu. All rights reserved.
+//
+
+import UIKit
+infix operator >>> { associativity left }
+func >>> <A, B, C>(f: B -> C, g: A -> B) -> A -> C {
+    return { x in f(g(x)) }
+}
+
+func foldr<U,T> (initial:U, function: (U,T) -> U, collection: Array<T> ) -> U {
+    var combined = initial
+    for object in collection {
+        combined = function(combined, object)
+    }
+    
+    return combined;
+    
+}
+
+
+func count(array: Array<Int>) -> Int {
+    return foldr(0, function: { (value, currentObject) -> Int in
+        return value + 1
+        }, collection: array)
+}
+
+
+func sum(array: Array<Int>) -> Int {
+    return foldr(0, function: { (x, y) -> Int in
+        return x + y
+        }, collection: array)
+}
+
+
+func map(array: Array<Int>) -> Array<Int> {
+    return foldr([], function: { (value, currentObject) -> Array<Int> in
+        var mutableArray:Array = value
+        mutableArray.append(currentObject  * 2)
+        return mutableArray
+        }, collection: array)
+}
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let array = [1, 2, 3, 4, 5, 6, 7]
+        
+        let mappedArray = map(array)
+        let numberOfObjects = count(array)
+        let sumOfObjects = sum(array)
+    }
+}
+
